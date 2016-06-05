e113123e7db2d57687d43330aa00bdb99ca60bef
Christopher Catilo <chriscatilo@hotmail.com>
2016-06-05 15:33:11 +0100

    Action Results

--- OdeToFood/OdeToFood/Controllers/CuisineController.cs
+++ OdeToFood/OdeToFood/Controllers/CuisineController.cs
@@ -7,6 +7,17 @@ namespace OdeToFood.Controllers
         public ActionResult Search(string name = "french")
         {
             var message = Server.HtmlEncode(name);
+
+            // return RedirectPermanent("http://microsoft.com");
+
+            // return RedirectToAction("Index", "Home", new { name = name });
+
+            // return RedirectToRoute("Default", new { controller = "Home", action = "About" });
+
+            // return File(Server.MapPath("~/Content/site.css), "text/css");
+
+            // return Json(new { Message = message, Name = "Scott" }, JsonRequestBehaviour.AllowsGet);
+
             return Content(message);
         }       
     }
