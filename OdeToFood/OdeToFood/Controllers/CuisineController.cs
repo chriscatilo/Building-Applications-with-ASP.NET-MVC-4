aeaf618d4e7a8cf83c3db74e29f6ff0bee795e1b
Christopher Catilo <chriscatilo@hotmail.com>
2016-06-05 15:52:51 +0100

    Action Selector Attributes

--- OdeToFood/OdeToFood/Controllers/CuisineController.cs
+++ OdeToFood/OdeToFood/Controllers/CuisineController.cs
@@ -4,21 +4,18 @@ namespace OdeToFood.Controllers
 {
     public class CuisineController : Controller
     {
+        [HttpPost] // can only be invoked on a POST request
         public ActionResult Search(string name = "french")
         {
             var message = Server.HtmlEncode(name);
 
-            // return RedirectPermanent("http://microsoft.com");
-
-            // return RedirectToAction("Index", "Home", new { name = name });
-
-            // return RedirectToRoute("Default", new { controller = "Home", action = "About" });
-
-            // return File(Server.MapPath("~/Content/site.css), "text/css");
-
-            // return Json(new { Message = message, Name = "Scott" }, JsonRequestBehaviour.AllowsGet);
-
             return Content(message);
-        }       
+        }
+
+        [HttpGet] // can only be invoked on a GET request
+        public ActionResult Search()
+        {
+            return Content("Search!");
+        }
     }
 }
